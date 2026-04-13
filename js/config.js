e97c2d478f0f2c8d9b9b1c4d4f18987534ab3114
708neo708neo-prog <708neo708neo@gmail.com>
2026-04-14 07:51:03 +0900

    Add Firebase config v1.5

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 // ===================================================
-// 設定ファイル - デプロイ時に以下の値を設定してください
+// 設定ファイル - Firebase設定
 // ===================================================
 
 var CONFIG = {
-  // Google Cloud Console で作成した OAuth 2.0 クライアントID
-  // 例: '123456789012-abcdefghijklmnop.apps.googleusercontent.com'
-  GOOGLE_CLIENT_ID: '594789726286-per5q3e4hev03nhfe7ao7ktchjbnhuf7.apps.googleusercontent.com',
-
-  // Google Apps Script をWebアプリとしてデプロイした際のURL
-  // 例: 'https://script.google.com/macros/s/AKfy.../exec'
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzEYlZGLh5tTHdciXrCZby6EGIO56QCQw4CQj8jjw9yrhtcAN4NSbI0USdFkDHO_L0KUQ/exec',
+  firebase: {
+    apiKey:            'AIzaSyCdVl7V7jMXjhIzYk_pYr8_zh5mxwqX7Fw',
+    authDomain:        'seisan-kanri-6ebf3.firebaseapp.com',
+    projectId:         'seisan-kanri-6ebf3',
+    storageBucket:     'seisan-kanri-6ebf3.firebasestorage.app',
+    messagingSenderId: '125702045437',
+    appId:             '1:125702045437:web:f4d26252cfc6fabe654894',
+  }
 };
